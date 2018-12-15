7edba1d5ce03d0f67d4623f2b8fdd5ba92dccb1e
wasanx25 <wasanx25@gmail.com>
2018-12-16 01:59:44 +0900

    Add receive message sample

---
+++ pull.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func main() {
+	home := os.Getenv("HOME")
+	s, _ := session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1"),
+		Endpoint: aws.String("http://localhost:4576"),
+		Credentials: credentials.NewSharedCredentials(home + "/.aws/credentials", "localstack"),
+	})
+
+	service := sqs.New(s)
+
+	params := &sqs.ReceiveMessageInput{
+		QueueUrl: aws.String("http://localhost:4576/queue/TEST"),
+	}
+
+	res, err := service.ReceiveMessage(params)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(res)
+}
